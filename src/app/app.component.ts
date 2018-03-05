e6b7fe2942f75851e209dc68de403700744bb73c
Unknown <javitsilva@gmail.com>
2018-03-04 19:25:48 -0600

    added active style to current page nav tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  private currentPath: String;
+
+  constructor(private location: Location) {}
+
+  isActive(tab: String): boolean {
+    var tabPath = '/' + tab;
+    if(this.currentPath.includes(tabPath))
+      return true;
+    else
+      return false;
+  }
+
+  ngOnInit() {
+    this.currentPath = this.location.path();
+  }
 }
